91ca01ab309868fd29e82fbba7c95a62df4190db
12kg1a0132 <bpunyamurthi.com>
2019-11-07 22:26:50 +0530

    initial commit

---
+++ src/springMVCweb/HomeController.java
@@ -0,0 +1,20 @@
+package springMVCweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/con")
+public class HomeController {
+	@RequestMapping("/show")
+	public ModelAndView ShowPage() {
+		ModelAndView m=new ModelAndView();
+		m.setViewName("Home");
+		
+		return m;
+		
+		
+	}
+
+}
